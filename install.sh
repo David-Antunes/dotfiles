3ab1875403d9d175f98673be461290f79dc3a0af
david-antunes <example@example.com>
2023-03-19 13:11:28 +0000

    Move files outside of folder

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-cat notion.info > /etc/yum.repos.d/notion-repackaged.repo
 dnf update
-dnf install timeshift gnome-tweaks openssl vim google-chrome dnf-plugins-core wget flameshot speedtest-cli distrobox tldr htop
+dnf -y install timeshift gnome-tweaks openssl neovim git-delta vim google-chrome dnf-plugins-core wget flameshot speedtest-cli distrobox tldr nvtop htop iftop iotop ether-wake tmux gimp
 
 # install docker
 dnf config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
 
-dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+dnf -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
 
 systemctl enable docker.service
 systemctl start docker.service
@@ -25,7 +24,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 dnf check-update
-dnf install code
+dnf -y install code
 
 systemctl enable sshd.service
 systemctl start sshd.service
@@ -35,19 +34,14 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak install flathub com.spotify.Client
 flatpak install flathub com.discordapp.Discord
 flatpak install flathub com.logseq.Logseq
+flatpak install flathub org.signal.Signal
 
 # install gitkraken
 
 wget https://release.gitkraken.com/linux/gitkraken-amd64.rpm
 
-dnf install ./gitkraken-amd64.rpm
+dnf -y install ./gitkraken-amd64.rpm
 rm gitkraken-amd64.rpm
 
 # install mailspring
-
-wget https://updates.getmailspring.com/download?platform=linuxRpm
-
-dnf install ./mailspring-1.10.8-0.1.x86_64.rpm
-
-
 
